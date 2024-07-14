80006e25344aacafd832522cf38402916edf4d74
tiwariParth <243mjparth@gmail.com>
2024-07-14 21:53:03 +0530

    chore: Add preline.js script to improve page performance

---
+++ src/components/SpeechRecognition/SpeechRecognition.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { IconMicrophone } from "@tabler/icons-react";
+
+const SpeechRecognitionComponent = ({ setSourceText }) => {
+  const { transcript, listening } = useSpeechRecognition();
+
+  useEffect(() => {
+    setSourceText(transcript);
+  }, [transcript, setSourceText]);
+
+  const handlingVoiceRecording = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening({
+        continuous: true,
+      });
+    }
+  };
+  return (
+    <div>
+      <IconMicrophone
+        size={22}
+        className="text-gray-400"
+        onClick={handlingVoiceRecording}
+      />
+    </div>
+  );
+};
+
+export default SpeechRecognitionComponent;
